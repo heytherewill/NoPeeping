e3c1a5548061dd1eed2476e7a1d466cda4996853
William Barbosa <william.sb@hotmail.com>
2018-05-14 21:03:48 +0200

    :iphone: Demo project

---
+++ NoPeeping.Demo/AppDelegate.fs
@@ -0,0 +1,14 @@
+﻿namespace NoPeeping.Demo
+
+open UIKit
+open Foundation
+open NoPeeping
+
+[<Register ("AppDelegate")>]
+type AppDelegate () =
+    inherit ScreenshotRemovingAppDelegate ()
+
+    override val Window = null with get, set
+
+    override this.FinishedLaunching (app, options) =
+        true
---
+++ NoPeeping.Demo/ViewController.fs
@@ -0,0 +1,13 @@
+﻿namespace NoPeeping.Demo
+
+open System
+open Foundation
+open UIKit
+
+[<Register ("ViewController")>]
+type ViewController (handle:IntPtr) =
+    inherit UIViewController (handle)
+
+    override this.ViewDidLoad () =
+        base.ViewDidLoad ()
+        this.View.BackgroundColor <- UIColor.Red
