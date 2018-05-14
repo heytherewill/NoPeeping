1ee185d1550f21e0a69017ae242e9e0e464de637
William Barbosa <william.sb@hotmail.com>
2018-05-14 21:03:25 +0200

    :eyeglasses: Initial implementation

---
+++ NoPeeping/ScreenshotRemovers.fs
@@ -0,0 +1,39 @@
+﻿namespace NoPeeping
+
+open System
+open UIKit
+
+type IScreenshotRemover =
+    abstract member OnResignActivation : (UIApplicationDelegate) -> unit
+
+    abstract member OnActivated : (UIApplicationDelegate) -> unit
+
+type BasicScreenshotRemover() =
+
+    interface IScreenshotRemover with
+
+        member this.OnResignActivation(appDelegate: UIApplicationDelegate) =
+            appDelegate.Window.Hidden <- true
+
+        member this.OnActivated(appDelegate: UIApplicationDelegate) =
+            appDelegate.Window.Hidden <- false
+        
+
+type ViewScreenshotRemover(view: UIView, useWindowFrame: bool) =
+
+    interface IScreenshotRemover with
+
+        member this.OnResignActivation(appDelegate: UIApplicationDelegate) =
+            if useWindowFrame then
+                view.Frame <- appDelegate.Window.Frame
+            appDelegate.Window.AddSubview(view)
+            appDelegate.Window.BringSubviewToFront(view)
+
+        member this.OnActivated(appDelegate: UIApplicationDelegate) =
+            view.RemoveFromSuperview()
+
+type ColorScreenshotRemover(color: UIColor) =
+    inherit ViewScreenshotRemover(new UIView(BackgroundColor = color), true)
+
+type ImageScreenshotRemover(image: UIImage) =
+    inherit ViewScreenshotRemover(new UIImageView(image), true)
---
+++ NoPeeping/ScreenshotRemovingAppDelegate.fs
@@ -0,0 +1,14 @@
+﻿namespace NoPeeping
+
+open UIKit
+
+type ScreenshotRemovingAppDelegate() =
+    inherit UIApplicationDelegate()
+
+    member val ScreenshotRemover : IScreenshotRemover = BasicScreenshotRemover() :> IScreenshotRemover with get, set
+
+    override this.OnResignActivation(application: UIApplication) =
+        this.ScreenshotRemover.OnResignActivation(this)
+
+    override this.OnActivated(application: UIApplication) =
+        this.ScreenshotRemover.OnActivated(this)
